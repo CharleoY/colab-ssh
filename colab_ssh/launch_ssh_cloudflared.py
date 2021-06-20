9ea4c3e7540ddcccdf3bfee634a9a9832561e0e3
yang <yangqi@mail.ru>
2021-06-20 18:46:29 +0300

    1

--- colab_ssh/launch_ssh_cloudflared.py
+++ colab_ssh/launch_ssh_cloudflared.py
@@ -89,47 +89,7 @@ def launch_ssh_cloudflared(
         print("DEBUG:", info)
 
     if info:
-        # print("Successfully running on ", "{}:{}".format(host, port))
-        if importlib.util.find_spec("IPython") and 'ipykernel' in sys.modules:
-            from IPython.display import display, HTML
-            display(HTML(render_template("launch_ssh_cloudflared.html", info)))
-        else:
-            print("Now, you need to setup your client machine by following these steps:")
-            print("""
-    1) Download Cloudflared (Argo Tunnel) from https://developers.cloudflare.com/argo-tunnel/getting-started/installation, then copy the absolute path to the cloudflare binary.
-    2) Append the following to your SSH config file (usually under ~/.ssh/config):
-
-        Host *.trycloudflare.com
-            HostName %h
-            User root
-            Port 22
-            ProxyCommand <PUT_THE_ABSOLUTE_CLOUDFLARE_PATH_HERE> access ssh --hostname %h
-
-*) Connect with SSH Terminal
-    To connect using your terminal, type this command:
-        ssh {domain}
-
-*) Connect with VSCode Remote SSH
-    You can also connect with VSCode Remote SSH (Ctrl+Shift+P and type "Connect to Host..."). Then, paste the following hostname in the opened command palette:
-        {domain}
-""".format(**info))
-
-    #     print("[Optional] You can also connect with VSCode SSH Remote extension by:")
-    #     print(f"""
-    # 1. Set the following configuration into your SSH config file (~/.ssh/config):
-
-    #     Host *.trycloudflare.com
-    #         HostName %h
-    #         User root
-    #         Port {port}
-    #         ProxyCommand <PUT_THE_ABSOLUTE_CLOUDFLARE_PATH_HERE> access ssh --hostname %h
-
-    # 2. Connect to Remote SSH on VSCode (Ctrl+Shift+P and type "Connect to Host...") and paste this hostname:
-    #     {host}
-    #     """)
-    #     print(f'''
-
-        #   ''')
+        return info
     else:
         print(proc.stdout.readlines())
         raise Exception(
